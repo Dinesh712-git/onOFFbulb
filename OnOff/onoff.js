89983b63c39c3cc1a98f4f6649a1165d7608dac9
Dinesh Madusanka <91844187+Dinesh712-git@users.noreply.github.com>
2023-02-25 22:19:16 +0530

    code

---
+++ OnOff/onoff.js
@@ -0,0 +1,18 @@
+ function onoff()
+            {
+                var im=document.getElementById('image');
+                if(im.src.match('on'))
+                {
+                    image.src="off.jpg";
+                  
+                }
+                else{
+                    image.src="on.jpg";
+                   
+                }
+                
+            }
+            
+           
+            
+            
---
+++ OnOff/sen.js
@@ -0,0 +1,9 @@
+var im=document.getElementById('image');
+                if(im.image="on.jpg")
+                {
+                    document.write("on click");
+                  
+                }
+                else{
+                    document.write("off click");
+                }
